e71d8f9d2bbdcc0ba4aae8ade707573237396154
kuikaopong <kuikaopong.2002@gmail.com>
2022-09-12 18:13:37 +0700

    fix some bugs

--- polls/models.py
+++ polls/models.py
@@ -3,18 +3,30 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_data = models.DateTimeField('data expired', null=True, blank=True)
+    end_date = models.DateTimeField('date expired', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
     
     def was_published_recently(self):
-        now = timezone.now()    
+        now = timezone.localtime()    
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """Return whether is it published."""
+        now = timezone.localtime()
+        return self.pub_date <= now
+    
+    def can_vote(self) -> bool:
+        """Current date is ready for voting."""
+        now = timezone.localtime()
+        if not self.end_date:
+            return self.is_published()
+        return self.is_published and now <= self.end_date
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -47,12 +47,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
